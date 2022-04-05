53d28e287c5a792bc89b7c6b7d72863917b27b18
Carlos Molina <carlosmolinaalcolea2000@gmail.com>
2022-04-05 18:52:56 +0200

    modelo fisico + relaciones de Eloquent

---
+++ app/Models/Color.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Color extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function products()
+    {
+        return $this->hasMany(Products::class);
+    }
+
+    public function sizes()
+    {
+        return $this->hasMany(Sizes::class);
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    public function brands()
+    {
+        return $this->belongsTo(Brands::class);
+    }
+
+    public function subcategories()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    public function colors()
+    {
+        return $this->belongsToMany(Colors::class);
+    }
+
+    public function sizes()
+    {
+        return $this->hasMany(Sizes::class);
+    }
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+}
