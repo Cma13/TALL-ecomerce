e6a66fa9c937e88e4a301940f46b38d367882566
Carlos M <carlosmolinaalcolea2000@gmail.com>
2022-04-25 20:00:16 +0200

    addCartItemSize funcionando

--- app/Http/Livewire/AddCartItemSize.php
+++ app/Http/Livewire/AddCartItemSize.php
@@ -2,10 +2,43 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Size;
 use Livewire\Component;
 
 class AddCartItemSize extends Component
 {
+    public $product, $sizes, $color_id = "";
+    public $size_id = "", $colors = [];
+    public $quantity = 0, $qty = 1;
+
+    public function updatedSizeId($sizeSelected)
+    {
+        $size = Size::find($sizeSelected);
+
+        $this->colors = $size->colors;
+    }
+    
+    public function updatedColorId($colorSelected)
+    {
+        $size = Size::find($this->size_id);
+        $this->quantity = $size->colors->find($colorSelected)->pivot->quantity;
+    }
+
+    public function decrement()
+    {
+        $this->qty = $this->qty - 1;
+    }
+
+    public function increment()
+    {
+        $this->qty = $this->qty + 1;
+    }
+
+    public function mount()
+    {
+        $this->sizes = $this->product->sizes;
+    }
+
     public function render()
     {
         return view('livewire.add-cart-item-size');
--- resources/views/livewire/add-cart-item-size.blade.php
+++ resources/views/livewire/add-cart-item-size.blade.php
@@ -1,3 +1,37 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
+<div x-data>
+    <div>
+        <p class="text-xl text-gray-700">Talla:</p>
+        <select wire:model="size_id" class="form-control w-full">
+            <option selected disabled value="">Seleccione una talla</option>
+            @foreach ($sizes as $size)
+                <option value="{{ $size->id }}">{{ $size->name }}</option>
+            @endforeach
+        </select>
+    </div>
+    <div class="mt-2">
+        <p class="text-xl text-gray-700">Color:</p>
+        <select wire:model="color_id" class="form-control w-full">
+            <option selected disabled value="">Seleccione un color</option>
+            @foreach ($colors as $color)
+                <option value="{{ $color->id }}">{{ __($color->name) }}</option>
+            @endforeach
+        </select>
+    </div>
+    <div class="flex mt-4">
+        <div class="mr-4">
+            <x-jet-secondary-button disabled x-bind:disabled="$wire.qty <= 1" wire:loading.attr="disabled"
+                wire:target="decrement" wire:click="decrement">
+                -
+            </x-jet-secondary-button>
+            <span class="mx-2 text-neutral-700">{{ $qty }}</span>
+            <x-jet-secondary-button disabled x-bind:disabled="$wire.qty >= $wire.quantity" wire:loading.attr="disabled"
+                wire:target="increment" wire:click="increment">
+                +
+            </x-jet-secondary-button>
+        </div>
+        <div class="flex-1">
+            <x-button x-bind:disabled="!$wire.quantity" color="orange" class="w-full ">Agregar a carrito de compras
+            </x-button>
+        </div>
+    </div>
 </div>
